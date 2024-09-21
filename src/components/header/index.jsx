c26420b3371dbf4ebd375a994963c75150f45f4b
Diego R R Silva <165964850+Decksx7@users.noreply.github.com>
2024-09-20 22:46:38 -0300

    Separando funcionalidades

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,172 +1,57 @@
-import { MagnifyingGlass, Star } from "phosphor-react";
-import { Gear, Bell, List } from "phosphor-react";
+
+import { MagnifyingGlass, Gear, Bell, List } from "phosphor-react";
 import img from "./img/music.png";
 import styles from "./header.module.css";
-import { useState, useEffect } from "react";
-import { api } from "../../services/api";
-import { STORAGE_SERVICE } from "../../services/storage";
-import { useNavigate } from "react-router-dom";
 
-export function Header() {
-  const navigate = useNavigate();
-  const [dado, setdado] = useState("");
-  const [datas, setdatas] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-  const [showFavorites, setShowFavorites] = useState(false);
-
-  const token =
-    "BQCe6PL7X32bnQYgpcirJgKUA5iewQh2zGMXJRR5qeW0mCW3fAp0jvVanonXErpYkjd97YziMo6PvPQUJDIjvmNBZVODvN9pqL97vX9zeCFIAoGsuQM";
-
-  function handleChangeSearchValue(e) {
-    setdado(e.target.value);
-  }
-
-  useEffect(() => {
-    console.log("data aqui ", datas);
-    console.log(datas.length);
-  }, [datas]);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const { data } = await api.get(`/v1/search?q=${dado}&type=track`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const tracks = data.tracks.items.map((item) => item);
-    setdatas(tracks);
-  }
-
-  const addSongToPlaylist = (song) => {
-    STORAGE_SERVICE.createPlaylist(song);
-    alert(`A música "${song.name}" foi adicionada à playlist!`);
-  };
-
-  const loadFavorites = () => {
-    const savedFavorites = STORAGE_SERVICE.listContacts();
-    setFavorites(savedFavorites);
-    setShowFavorites(true);
-  };
-
-  const resetState = () => {
-    setdado("");
-    setdatas([]);
-    setShowFavorites(false);
-    navigate("/");
-  };
-
+export function Header({
+  handleChangeSearchValue,
+  loadFavorites,
+  resetState,
+  dado,
+  handleSubmit,
+}) {
   return (
-    <>
-      <header className={styles.header}>
-        <div className={styles["left-container"]}>
-          <img
-            src={img}
-            alt="Music"
-            className={styles.iconImage}
-            onClick={resetState}
-            style={{ cursor: "pointer" }}
+    <header className={styles.header}>
+      <div className={styles["left-container"]}>
+        <img
+          src={img}
+          alt="Music"
+          className={styles.iconImage}
+          onClick={resetState}
+          style={{ cursor: "pointer" }}
+        />
+        <div className={styles.searchBar}>
+          <MagnifyingGlass size={20} className={styles.icon} />
+          <input
+            type="text"
+            placeholder="Search for songs, artists, etc."
+            className={styles.input}
+            onChange={handleChangeSearchValue}
+            value={dado}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSubmit(e);
+              }
+            }}
           />
-          <div className={styles.searchBar}>
-            <MagnifyingGlass size={20} className={styles.icon} />
-            <input
-              type="text"
-              placeholder="Search for songs, artists, etc."
-              className={styles.input}
-              onChange={handleChangeSearchValue}
-              value={dado}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  handleSubmit(e);
-                }
-              }}
-            />
-          </div>
         </div>
-        <div className={styles["right-container"]}>
-          <button onClick={loadFavorites} className={styles.premiumButton}>
-            Favoritos
+      </div>
+      <div className={styles["right-container"]}>
+        <button onClick={loadFavorites} className={styles.premiumButton}>
+          Favoritos
+        </button>
+        <div className={styles.iconsGroup}>
+          <button>
+            <Gear size={30} className={styles.iconGear} />
           </button>
-          <div className={styles.iconsGroup}>
-            <button>
-              <Gear size={30} className={styles.iconGear} />
-            </button>
-            <button>
-              <Bell size={30} className={styles.iconBell} />
-            </button>
-            <button>
-              <List size={30} className={styles.iconList} />
-            </button>
-          </div>
+          <button>
+            <Bell size={30} className={styles.iconBell} />
+          </button>
+          <button>
+            <List size={30} className={styles.iconList} />
+          </button>
         </div>
-      </header>
-      <main>
-        <div className={styles.musics}>
-          {showFavorites ? (
-            <ul className={styles.album}>
-              {favorites.map((favorite, index) => (
-                <li key={index} style={{ marginTop: "20px" }}>
-                  <img
-                    className={styles.img1}
-                    src={favorite.imagem}
-                    alt="Foto do album"
-                  />
-                  <section className={styles.content}>
-                    <div className={styles.textContent}>
-                      <h1>{favorite.name}</h1>
-                      <p>{favorite.artista}</p>
-                      <div className={styles.favorite}>
-                        <audio controls>
-                          <source src={favorite.audio} type="audio/mpeg" />
-                          Your browser does not support the audio element.
-                        </audio>
-                        <button onClick={() => addSongToPlaylist(favorite)}>
-                          <Star size={32} color="#c8cb1a" weight="fill" />
-                        </button>
-                      </div>
-                    </div>
-                  </section>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <form onSubmit={handleSubmit}>
-              {datas.length > 0 && (
-                <ul className={styles.album}>
-                  {datas.map((data, index) => (
-                    <li key={index} style={{ marginTop: "20px" }}>
-                      <img
-                        className={styles.img1}
-                        src={data.album.images[0].url}
-                        alt={data.album.name}
-                      />
-                      <section className={styles.content}>
-                        <div className={styles.textContent}>
-                          <h1>{data.name}</h1>
-                          <p>{data.artists[0].name}</p>
-                          <div className={styles.favorite}>
-                            <audio controls>
-                              <source
-                                src={data.preview_url}
-                                type="audio/mpeg"
-                              />
-                              Your browser does not support the audio element.
-                            </audio>
-                            <button onClick={() => addSongToPlaylist(data)}>
-                              <Star size={32} color="#c8cb1a" weight="fill" />
-                            </button>
-                          </div>
-                        </div>
-                      </section>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </form>
-          )}
-        </div>
-      </main>
-    </>
+      </div>
+    </header>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,138 @@
-import { Header } from "../../components/header"
-export function Home() {
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Header } from "../../components/header";
+import { Star } from "phosphor-react";
+import styles from "./home.module.css";
+import { api } from "../../services/api";
+import { STORAGE_SERVICE } from "../../services/storage";
 
-  
+export function Home() {
+  const navigate = useNavigate();
+  const [dado, setDado] = useState("");
+  const [datas, setDatas] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false);
+
+  const token =
+    "BQBvrLG1Mhd0IjV6BKvkHKtEifCpZWZNSPu39RJCuNfgkt98Eg4OLwHe-lewsPJWIZ0bXQErnq-OtiNR-8nqdpKp8gJJOmo7akOx_tIx6roNHxaAdZo";
+
+  function handleChangeSearchValue(e) {
+    setDado(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log("Dados aqui: ", datas);
+    console.log("Quantidade de dados: ", datas.length);
+  }, [datas]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const { data } = await api.get(`/v1/search?q=${dado}&type=track`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const tracks = data.tracks.items.map((item) => item);
+      setDatas(tracks);
+    } catch (error) {
+      console.error("Erro ao buscar dados: ", error);
+    }
+  }
+
+  const addSongToPlaylist = (song) => {
+    STORAGE_SERVICE.createPlaylist(song);
+    alert(`A música "${song.name}" foi adicionada à playlist!`);
+  };
+
+  const loadFavorites = () => {
+    const savedFavorites = STORAGE_SERVICE.listContacts();
+    setFavorites(savedFavorites);
+    setShowFavorites(true);
+  };
+
+  const resetState = () => {
+    setDado("");
+    setDatas([]);
+    setShowFavorites(false);
+    navigate("/");
+  };
+
   return (
     <div>
-      <Header />
+      <Header
+        handleChangeSearchValue={handleChangeSearchValue}
+        loadFavorites={loadFavorites}
+        resetState={resetState}
+        dado={dado}
+        handleSubmit={handleSubmit}
+      />
+      <main>
+        <div className={styles.musics}>
+          {showFavorites ? (
+            <ul className={styles.album}>
+              {favorites.map((favorite, index) => (
+                <li key={index} style={{ marginTop: "20px" }}>
+                  <img
+                    className={styles.img1}
+                    src={favorite.imagem}
+                    alt="Foto do álbum"
+                  />
+                  <section className={styles.content}>
+                    <div className={styles.textContent}>
+                      <h1>{favorite.name}</h1>
+                      <p>{favorite.artista}</p>
+                      <div className={styles.favorite}>
+                        <audio controls>
+                          <source src={favorite.audio} type="audio/mpeg" />
+                          Seu navegador não suporta o elemento de áudio.
+                        </audio>
+                        <button onClick={() => addSongToPlaylist(favorite)}>
+                          <Star size={32} color="#c8cb1a" weight="fill" />
+                        </button>
+                      </div>
+                    </div>
+                  </section>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              {datas.length > 0 && (
+                <ul className={styles.album}>
+                  {datas.map((data, index) => (
+                    <li key={index} style={{ marginTop: "20px" }}>
+                      <img
+                        className={styles.img1}
+                        src={data.album.images[0].url}
+                        alt={data.album.name}
+                      />
+                      <section className={styles.content}>
+                        <div className={styles.textContent}>
+                          <h1>{data.name}</h1>
+                          <p>{data.artists[0].name}</p>
+                          <div className={styles.favorite}>
+                            <audio controls>
+                              <source
+                                src={data.preview_url}
+                                type="audio/mpeg"
+                              />
+                              Seu navegador não suporta o elemento de áudio.
+                            </audio>
+                            <button onClick={() => addSongToPlaylist(data)}>
+                              <Star size={32} color="#c8cb1a" weight="fill" />
+                            </button>
+                          </div>
+                        </div>
+                      </section>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </form>
+          )}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
